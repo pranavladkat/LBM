d9636f6457d99cd78144b883d7500c397a74ad2e
Pranav Ladkat <pranavladkat@gmail.com>
2015-03-22 22:57:31 -0400

    added D2Q9 model constants

--- src/cavity_flow.cpp
+++ src/cavity_flow.cpp
@@ -3,17 +3,23 @@
 
 using namespace std;
 
-const int IMAX = 10;
+const int IMAX = 100;
 const int JMAX = IMAX;
 
 const double tau = 2./3.;
-const double Re = 1000;
+const double Re = 100;
 
 const double u_lid = Re*(tau-0.5)/(3.*(IMAX-1.));
 
 
 int main()
 {
+    // directional weights for f_eq (for D2Q9)
+    const vector<double> weight = {4./9.,1./9.,1./9.,1./9.,1./9.,1./36.,1./36.,1./36.,1./36.};
+
+    // particle velocity
+    const vector<vector<double>> e = {{0,0},{1,0},{0,1},{-1,0},{0,-1},{1,1},{-1,1},{-1,-1},{1,-1}};
+
     // define variables
     vector<vector<vector<double>>> f       (IMAX,vector<vector<double>>(JMAX,vector<double>(9,0)));
     vector<vector<vector<double>>> f_str   (IMAX,vector<vector<double>>(JMAX,vector<double>(9,0)));
@@ -24,6 +30,13 @@ int main()
     vector<vector<double>>         rho_old (IMAX,vector<double>(JMAX,0));
     vector<vector<double>>         u_old   (IMAX,vector<double>(JMAX,0));
     vector<vector<double>>         v_old   (IMAX,vector<double>(JMAX,0));
+
+
+    // print setup values
+    cout << "Reynolds number = " << Re << endl;
+    cout << "Lid Velocity = " << u_lid << endl;
+    cout << "Tau = " << tau << endl;
+    cout << "Grid size = " << IMAX << " x " << JMAX << endl;
 
 
     cout << "Hello World!" << endl;
